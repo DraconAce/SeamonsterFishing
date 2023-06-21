85a6d5ede47eaed412d605082421a7270b609882
LunisLuminous <Linusxyz@gmx.net>
2023-06-22 00:58:36 +0200

    [Added] Lightning functionality
    Settings in Environment -> Lightning
    
    FMOD Event "Lightning" also plays (currently no sound added into the event)
    
    TODO: VFX of lightning

---
+++ Assets/Scripts/FX/LightningManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Lightning_Manager : MonoBehaviour
+{
+    
+    [SerializeField] private Light spotLight;
+    
+    [SerializeField] private FMODUnity.EventReference LightningSound;
+
+    private Sequence lightningSequence;
+    [Header("Flash Settings")] 
+    [SerializeField] float minCooldown = 2f;
+    [SerializeField] float maxCooldown = 5f;
+    [SerializeField] private float targetIntensity = 100f;
+    [SerializeField] private float keepLightningOnDuration = 0.2f;
+    [SerializeField] private TweenSettings lightningSettings;
+
+    private bool lightningIsActive;
+    private IEnumerator lightningCoroutine;
+
+    private void Start() 
+    {
+        lightningIsActive = true;
+        lightningCoroutine = DoLightning();
+        StartCoroutine(lightningCoroutine);
+    }
+
+    private IEnumerator DoLightning() 
+    { 
+        while (lightningIsActive)
+        {
+            float waitTime = Random.Range(minCooldown, maxCooldown);
+            //Debug.Log("Lightning wait:" + waitTime);
+            yield return new WaitForSeconds(waitTime);
+            createLightning();
+        }
+    }
+
+    void createLightning()
+    {
+        lightningSequence = DOTween.Sequence();
+        lightningSequence.Append(LightningTween(targetIntensity, lightningSettings));
+        lightningSequence.AppendInterval(keepLightningOnDuration);
+        lightningSequence.Append(LightningTween(0f, lightningSettings));
+
+        FMODUnity.RuntimeManager.PlayOneShot(LightningSound);
+    }
+
+    private Tween LightningTween(float targetIntensity, TweenSettings targetSettings)
+    {
+        return spotLight.DOIntensity(targetIntensity, targetSettings.Duration)
+            .SetEase(targetSettings.TweenEase)
+            .OnStart(() => targetSettings.OnStartAction?.Invoke())
+            .OnComplete(() => targetSettings.OnCompleteAction?.Invoke());
+    }
+
+    private void OnDestroy()
+    {
+        lightningSequence?.Kill();
+    }
+
+}
