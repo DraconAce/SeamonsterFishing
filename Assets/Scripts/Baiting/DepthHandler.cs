f04eca8c5d3259260aa09f31f80ee9644fe0c3cf
Charlien Fecher <72200897+DraconAce@users.noreply.github.com>
2023-07-08 15:27:58 +0200

    [Added] Environmental progression (Water changes color) [Adjusted] Rock wall behind player more visible

--- Assets/Scripts/Baiting/DepthHandler.cs
+++ Assets/Scripts/Baiting/DepthHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -7,15 +8,42 @@ public class DepthHandler : MonoBehaviour, IManualUpdateSubscriber, IMovePerSeco
     [SerializeField] private float meterPerSecond = 0.1f;
     [SerializeField] private TextMeshProUGUI depthText;
     [SerializeField] private Transform waterSurface;
+    [SerializeField] private float[] depthThresholds;
+    
     public float MovePerSecond => meterPerSecond;
+
+    private float currentDepth;
+    public float CurrentDepth
+    {
+        get => currentDepth;
+        private set
+        {
+            var formerValue = currentDepth;
+            
+            currentDepth = value;
+
+            if (Mathf.Approximately(formerValue, currentDepth)
+                || currentDepth <= depthThresholds[currentDepthThresholdIndex]) return;
+            
+            if(currentDepthThresholdIndex < depthThresholds.Length-1)
+                currentDepthThresholdIndex++;
+            
+            DepthThresholdChangedEvent?.Invoke(currentDepthThresholdIndex);
+        }
+    }
 
     private UpdateManager updateManager;
     private float timer;
+    private int currentDepthThresholdIndex;
+
+    public event Action<int> DepthThresholdChangedEvent;
 
     private void Start()
     {
         updateManager = UpdateManager.instance;
         updateManager.SubscribeToManualUpdate(this);
+        
+        currentDepthThresholdIndex = 0;
     }
 
     public void ManualUpdate()
@@ -28,9 +56,9 @@ public class DepthHandler : MonoBehaviour, IManualUpdateSubscriber, IMovePerSeco
     {
         timer += Time.deltaTime;
 
-        var currentDepth = CalculateCurrentDepth();
+        CurrentDepth = CalculateCurrentDepth();
         
-        depthText.text = "Depth: " + currentDepth;
+        depthText.text = "Depth: " + CurrentDepth;
     }
 
     private int CalculateCurrentDepth() => (int) Mathf.Round(timer * meterPerSecond + depthOffset);
@@ -39,7 +67,7 @@ public class DepthHandler : MonoBehaviour, IManualUpdateSubscriber, IMovePerSeco
     {
         var currentSurfacePosition = waterSurface.position;
         
-        waterSurface.position = currentSurfacePosition + Vector3.up * Time.deltaTime * meterPerSecond;
+        waterSurface.position = currentSurfacePosition + Vector3.up * (Time.deltaTime * meterPerSecond);
     }
     
     public void ResetTimer() => timer = 0;
---
+++ Assets/Scripts/Baiting/EnvironmentProgressController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using Suimono.Core;
+using UnityEngine;
+
+public class EnvironmentProgressController : MonoBehaviour
+{
+    [SerializeField] private DepthHandler depthHandler;
+    [SerializeField] private SuimonoObject suimonoObject;
+    [SerializeField] private float colorTransitionTime;
+    [SerializeField] private Ease colorTransitionEase;
+    [SerializeField] private List<Color> depthEnvironmentColors;
+
+    private void Start()
+    {
+        SetWaterDepthColor(0);
+        
+        depthHandler.DepthThresholdChangedEvent += OnDepthThresholdChanged;
+    }
+
+    private void SetWaterDepthColor(int depthIndex) => suimonoObject.underwaterColor = depthEnvironmentColors[depthIndex];
+
+    private void OnDepthThresholdChanged(int depthIndex)
+    {
+        StartColorChangeTween(depthEnvironmentColors[depthIndex]);
+    }
+
+    private void StartColorChangeTween(Color targetColor)
+    {
+        var startColor = suimonoObject.underwaterColor;
+        
+        DOVirtual.Float(0, 1, colorTransitionTime, value => LerpColor(startColor, targetColor, value))
+            .SetEase(colorTransitionEase);
+    }
+
+    private void LerpColor(Color startColor, Color targetColor, float lerpValue) 
+        => suimonoObject.underwaterColor = Color.Lerp(startColor, targetColor, lerpValue);
+}
