b964105718aeef1ab72590dfd53d0268cba3e5be
Charlien Fecher <72200897+DraconAce@users.noreply.github.com>
2023-07-26 00:23:13 +0200

    [Added] New Node Scriptables for monster behaviour loop [Removed] Unused BehaviourTree Script

--- Assets/Scripts/Monster/BehaviourTree/BehaviourTree.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Behaviour Tree", menuName = "BehaviourTree/Create Behaviour Tree", order = 0)]
-public class BehaviourTree : ScriptableObject
-{
-    [SerializeField] private AbstractBehaviourTreeNode rootNode;
-    
-    public AbstractBehaviourTreeNode RootNode => rootNode;
-}
