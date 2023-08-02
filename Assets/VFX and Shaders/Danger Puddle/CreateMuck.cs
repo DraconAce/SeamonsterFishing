b4b0978f348298d6f12c572c81db3b137656ff4b
LunisLuminous <Linusxyz@gmx.net>
2023-08-02 03:04:30 +0200

    [Added] Turning Muck towards player
    rotate Muck_Spew towards player and set Transform of Muck_Explosion (and Puddle) to current player position

--- Assets/VFX and Shaders/Danger Puddle/CreateMuck.cs
+++ Assets/VFX and Shaders/Danger Puddle/CreateMuck.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class CreateMuck : MonoBehaviour
 {
-
+    
+    [SerializeField] private GameObject Muck_Spew_Gameobject;
     [SerializeField] private GameObject Muck_Explosion_Gameobject;
     private ParticleSystem Muck_Explosion;
     [SerializeField] private GameObject GooParticles_Gameobject;
     [SerializeField] private GameObject FireParticles_Gameobject;
+    private Transform playerTransform;
     
     void Start()
     {
         Muck_Explosion = Muck_Explosion_Gameobject.GetComponent<ParticleSystem>();
+        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
         StartCoroutine(MuckTimeTracker());
     }
     
     IEnumerator MuckTimeTracker()
     {
+        //rotate Muck_Spew towards player
+        var direction = (playerTransform.position - Muck_Spew_Gameobject.transform.position).normalized;
+        var targetRotation = Quaternion.LookRotation(direction, Vector3.forward);
+        Muck_Spew_Gameobject.transform.rotation = targetRotation;
+        
+        //set Transform of Muck_Explosion (and Puddle) to current player position
+        Muck_Explosion_Gameobject.transform.position = playerTransform.position;
+        
         //wait for play MuckExplosion
         yield return new WaitForSeconds(0.9f);
         Muck_Explosion.Play();
