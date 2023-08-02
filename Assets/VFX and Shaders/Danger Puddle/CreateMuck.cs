6d01a5a619938e3b96d34c2f6b4c6f925b38de1c
LunisLuminous <Linusxyz@gmx.net>
2023-08-02 02:27:10 +0200

    [Added] simple script to test Muck-Sequence

--- Assets/VFX and Shaders/Danger Puddle/CreateMuck.cs
+++ Assets/VFX and Shaders/Danger Puddle/CreateMuck.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CreateMuck : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private GameObject Muck_Explosion_Gameobject;
+    private ParticleSystem Muck_Explosion;
+    [SerializeField] private GameObject GooParticles_Gameobject;
+    [SerializeField] private GameObject FireParticles_Gameobject;
+    
     void Start()
     {
-        
+        Muck_Explosion = Muck_Explosion_Gameobject.GetComponent<ParticleSystem>();
+        StartCoroutine(MuckTimeTracker());
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    IEnumerator MuckTimeTracker()
     {
+        //wait for play MuckExplosion
+        yield return new WaitForSeconds(0.9f);
+        Muck_Explosion.Play();
         
+        //short wait for activate Muck_Puddle
+        yield return new WaitForSeconds(0.1f);
+        GooParticles_Gameobject.SetActive(true);
+        
+        //Testing fire after 2s
+        yield return new WaitForSeconds(2f);
+        FireParticles_Gameobject.SetActive(true);
     }
+    
 }
