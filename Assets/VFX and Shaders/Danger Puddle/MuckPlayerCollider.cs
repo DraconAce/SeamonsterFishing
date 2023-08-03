2839d6b315eb904855655980532f3109bd479cca
LunisLuminous <Linusxyz@gmx.net>
2023-08-03 11:34:13 +0200

    [Added] Boat in Fire burns over time
    resets after leaving

--- Assets/VFX and Shaders/Danger Puddle/MuckPlayerCollider.cs
+++ Assets/VFX and Shaders/Danger Puddle/MuckPlayerCollider.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class MuckPlayerCollider : MonoBehaviour
 {
-    
     [SerializeField] private GameObject DriveStation;
     private DriveStation_Moving DriveScript;
     [SerializeField] private FMODUnity.EventReference BoatFireHitSound;
+    
+    //Boat burning particles
+    [SerializeField] private GameObject FireParticles;
+    private GameObject FirstBoatBurn;
+    private GameObject SecondBoatBurn;
+    private GameObject ThirdBoatBurn;
+    [SerializeField] private float fireHeight = 0.5f;
+    
+    private IEnumerator boatBurningCoroutine;
     
     // Start is called before the first frame update
     void Start()
@@ -26,7 +34,8 @@ public class MuckPlayerCollider : MonoBehaviour
             {
                 //Debug.Log("Destroy Player because of Fire");
                 FMODUnity.RuntimeManager.PlayOneShot(BoatFireHitSound, other.ClosestPoint(transform.position));
-                GameStateManager.instance.ChangeGameState(GameState.Dead);
+                boatBurningCoroutine = DoBoatBurning();
+                StartCoroutine(boatBurningCoroutine);
             }
             
         }
@@ -36,8 +45,44 @@ public class MuckPlayerCollider : MonoBehaviour
     {
         if(other.gameObject.tag == "MuckPuddle")
         {
-            //slow down player
+            //stop slowing down player
             DriveScript.SetBoatInMuck(false);
+            
+            if (other.transform.GetChild(3).gameObject.activeSelf) //child3 is FireParticles
+            {
+                StopCoroutine(boatBurningCoroutine);
+                Destroy(FirstBoatBurn);
+                Destroy(SecondBoatBurn);
+                Destroy(ThirdBoatBurn);
+            }
         }
     }
+    
+    private IEnumerator DoBoatBurning() 
+    { 
+        //Debug.Log("Boat is burning");
+        
+        //instant fire
+        FirstBoatBurn = (GameObject)Instantiate(FireParticles, transform.position + new Vector3(0, fireHeight, 0), Quaternion.identity);
+        FirstBoatBurn.transform.parent = transform;
+        
+        yield return new WaitForSeconds(0.6f);
+        
+        //more fire
+        //Debug.Log("Burning State 2");
+        SecondBoatBurn = (GameObject)Instantiate(FireParticles, transform.position + new Vector3(0, 2*fireHeight, 0), Quaternion.identity);
+        SecondBoatBurn.transform.parent = transform;
+        
+        yield return new WaitForSeconds(0.6f);
+        
+        //more fire
+        //Debug.Log("Burning State 3");
+        ThirdBoatBurn = (GameObject)Instantiate(FireParticles, transform.position + new Vector3(0, 3*fireHeight, 0), Quaternion.identity);
+        ThirdBoatBurn.transform.parent = transform;
+        
+        yield return new WaitForSeconds(0.8f);
+        //Debug.Log("Boat is DEAD");
+        GameStateManager.instance.ChangeGameState(GameState.Dead);
+    }
+    
 }
