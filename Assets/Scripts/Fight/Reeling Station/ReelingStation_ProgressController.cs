858968484d7dc5523e8406b2fdd20f23a56c79a8
LunisLuminous <Linusxyz@gmx.net>
2023-08-08 15:48:42 +0200

    [Added] Reeling Sound
    using the Reeling Station_Progress Controller

--- Assets/Scripts/Fight/Reeling Station/ReelingStation_ProgressController.cs
+++ Assets/Scripts/Fight/Reeling Station/ReelingStation_ProgressController.cs
@@ -1,9 +1,13 @@
 using DG.Tweening;
 using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
 
 public class ReelingStation_ProgressController : AbstractStationSegment
 {
     [SerializeField] private GameObject reelingUI;
+    [SerializeField] private EventReference ReelingSound;
+    private EventInstance reelingSoundInstance;
     
     private ReelingStation reelingStation => (ReelingStation) ControllerStation;
     
@@ -13,11 +17,18 @@ public class ReelingStation_ProgressController : AbstractStationSegment
         
         reelingStation.OnReelingStartedEvent += OnReelingStarted;
         reelingStation.OnReelingCompletedEvent += OnReelingCompleted;
+        
+        reelingSoundInstance = RuntimeManager.CreateInstance(ReelingSound);
     }
     
-    private void OnReelingStarted() => reelingUI.SetActive(true);
+    private void OnReelingStarted() 
+    {
+        reelingUI.SetActive(true);
+        reelingSoundInstance.start();
+    } 
     private void OnReelingCompleted()
     {
+        reelingSoundInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         DOVirtual.DelayedCall(reelingStation.DelayForSubStationsOnReelingCompleted, 
             () => reelingUI.SetActive(false));
     }
