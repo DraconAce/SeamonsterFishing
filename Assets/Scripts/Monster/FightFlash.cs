5f8a68b5330f2777dcf9b8ef09a03a776e8dce82
LunisLuminous <Linusxyz@gmx.net>
2023-08-10 11:48:10 +0200

    [Added] LampCooldown Tween
    currently set Cooldown Ease to "Out Expo"

--- Assets/Scripts/Monster/FightFlash.cs
+++ Assets/Scripts/Monster/FightFlash.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using FMOD.Studio;
 using FMODUnity;
+using DG.Tweening;
 
 public class FightFlash : SpotFlash
 {
     [SerializeField] private GameObject Lamp;
     [SerializeField] private Color LampOffEmissionColor;
     [SerializeField] private float LampEmissionStrength = 8f;
+    [SerializeField] private Ease CooldownEase;
     private Material lampMat;
+    private float lampCooldown;
     
     public EventReference flashSound;
     public EventReference reloadFlashSound;
@@ -26,6 +29,7 @@ public class FightFlash : SpotFlash
         
         base.Start();
         
+        lampCooldown = base.GetCoolDownTimer();
         //get Lamp Material
         lampMat = Lamp.GetComponent<Renderer>().material;
         lampMat.SetColor("_EmissionColor", LampOffEmissionColor*LampEmissionStrength);
@@ -40,6 +44,8 @@ public class FightFlash : SpotFlash
         
         //turn on lamp cooldown glow
         lampMat.EnableKeyword("_EMISSION");
+        //start Tween to reduce Emission strength
+        lampMat.DOColor(LampOffEmissionColor, "_EmissionColor", lampCooldown).SetEase(CooldownEase); //Set Emission to 0 over time
     }
 
     protected override void FlashIsRecharged()
@@ -50,8 +56,9 @@ public class FightFlash : SpotFlash
         //play sound to signal that flash is reloaded
         reloadFlashSoundInstance.start();
         
-        //turn off lamp cooldown glow
+        //reset lamp cooldown glow 
         lampMat.DisableKeyword("_EMISSION");
+        lampMat.SetColor("_EmissionColor", LampOffEmissionColor*LampEmissionStrength);
         
         base.FlashIsRecharged();
     }
